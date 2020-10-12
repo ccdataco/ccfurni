bf4b69b484a529d7797aed8128b1fa600e175b29
HungVyDS <nguyenhungvy090499@gmail.com>
2020-10-12 19:08:13 +0700

    100% responsive for maipage. 20% responsive for products. 50% responsive for room. 20% responsive for sale

--- Project/furnitureCompany/furnitureApp/urls.py
+++ Project/furnitureCompany/furnitureApp/urls.py
@@ -2,5 +2,17 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('',views.mainpage),
+    path('home',views.home, name = 'home'),
+
+    path('about_us',views.about_us, name = 'about_us'),
+    path('products',views.products, name = 'products'),
+    path('rooms',views.rooms, name = 'rooms'),
+    # path('products',views.products),
+    # path('rooms',views.rooms),
+    # path('sale',views.sale),
+    # path('for_b2bc',views.for_b2bc),
+    # path('cart',views.cart),
+    # path('account',views.account),
+    
     
 ]
--- Project/furnitureCompany/furnitureApp/views.py
+++ Project/furnitureCompany/furnitureApp/views.py
@@ -3,3 +3,11 @@ from django.shortcuts import render
 # Create your views here.
 def mainpage(request):
     return render(request,'page/mainpage.html')
+def home(request):
+    return render(request,'page/mainpage.html')
+def about_us(request):
+    return render(request,'page/about_us.html')
+def products(request):
+    return render(request,'page/products.html')
+def rooms(request):
+    return render(request,'page/rooms.html')
